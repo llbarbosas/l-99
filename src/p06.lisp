44f2adaa617cff094167445e03134ce057b7f003
Lucas Barbosa <llbarbosasc@gmail.com>
2021-04-24 21:39:45 -0400

    fix(p06): palindromos de 2 letras ou menos

--- src/p06.lisp
+++ src/p06.lisp
@@ -2,10 +2,12 @@
 ;     (equal word (reverse word)))
 
 (defun is-palindrome (word)
+    (if (or (null word) (= (length word)))
+        t
     (let* ((size (length word)) 
             (cstart (char word 0)) 
             (cend (char word (- size 1))))
         (cond
-            ((= size 1) t)
             ((string/= cstart cend) nil)
-            (t (is-palindrome (subseq word 1 (- size 1)))))))
+            ((<= size 2) (string= cstart cend))
+            (t (is-palindrome (subseq word 1 (- size 1))))))))
